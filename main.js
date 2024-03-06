008529bf6ef8dba1e889ccbd661e55746c6099e9
Asifajaz <159148615+Asifajaz@users.noreply.github.com>
2024-03-06 15:00:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// let fullname = "rebort"
+// console.log (fullname)
+// let a : number = 1
+// let b : number = 2
+// console.log ("a + b=", a + b);
+// console.log ("a - b=", a - b);
+// console.log ("a / b=", a / b);
+// console.log ("a * b=", a * b);
+// console.log ("a % b=", a % b);
+// console.log ("a ** b=", a ** b);
+// let fullname = "asif"
+// console.log (fullname)
+// let a : number  = 2
+// let b : number  = 2
+// console.log ("a + b =", a + b);
+// console.log ("a - b =" , a - b);
+// console.log ("a * b =", a * b);
+// console.log ("a % b =", a % b);
+// console.log ("a / b=", a / b);
+// let fullname = "lucky"
+// console.log (fullname)
+// let a : number = 3
+// let b : number = 3
+// console.log ("a + b =", a + b);
+// console.log ("a - b =", a - b);
+// console.log ("a * b =", a * b);
+// console.log ("a % b =", a % b);
+// console.log ("a / b =", a / b);
+// console.log ("a ** b =", a ** b);
+var fullname = "Asif-Ajaz";
+console.log(fullname);
+var a = 2;
+var b = 2;
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a % b =", a % b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
